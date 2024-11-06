4a9b23b48010192a9b859babc30228c56f88debf
Dennis Kuijs <d.kuijs@student.fontys.nl>
2024-11-06 21:01:28 +0100

    added multi factor authentication

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-const TradeOfferManager = require('steam-tradeoffer-manager');
 const SteamUser = require('steam-user');
-const SteamCommunity = require('steamcommunity');
+const SteamTotp = require('steam-totp');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 let client = new SteamUser();
-let manager = new TradeOfferManager({
-    "steam": client,
-    "domain": "localhost",
-    "language": "en"
-})
-
 let loginOptions = {
     accountName: process.env.STEAM_USERNAME,
     password: process.env.STEAM_PASSWORD,
+	twoFactorCode: SteamTotp.generateAuthCode(process.env.STEAM_2FA_SHARED_SECRET)
 }
-
 client.logOn(loginOptions);
 
-client.on('loggedOn', function() {
+client.on('loggedOn', async function() {
     console.log('[BOT] Logged into Steam Account ' + client.steamID.getSteam3RenderedID());
     client.setPersona(SteamUser.EPersonaState.Online);
-});
-
-manager.on('newOffer', function(offer) {
-	console.log("New offer #" + offer.id + " from " + offer.partner.getSteam3RenderedID());
-	offer.accept(function(err, status) {
-		if (err) {
-			console.log("Unable to accept offer: " + err.message);
-		} else {
-			console.log("Offer accepted: " + status);
-			if (status == "pending") {
-				community.acceptConfirmationForObject("identitySecret", offer.id, function(err) {
-					if (err) {
-						console.log("Can't confirm trade offer: " + err.message);
-					} else {
-						console.log("Trade offer " + offer.id + " confirmed");
-					}
-				});
-			}
-		}
-	});
 });
